4a7fa4fe8ec35feb88560b71e29150129efc571e
Daniel Morante <daniel@morante.net>
2026-07-26 17:42:04 -0400

    mail/secure*: update lib hash + distinfo for privilege drop (Phase 7)

--- mail/securedmarc/Makefile.zig
+++ mail/securedmarc/Makefile.zig
@@ -1,1 +1,1 @@
-ZIG_TUPLE=	securemilter:pacyworld.dev/securemessage/securemilter-lib/archive/v0.3.0.tar.gz?dummy=/securemilter-lib-0.3.0.tar.gz:securemilter_lib-0.3.0-IbMuLEPGAQAGYFUm5yZUDDaaNKqHA5bQVL1JPnSnEcCg
+ZIG_TUPLE=	securemilter:pacyworld.dev/securemessage/securemilter-lib/archive/v0.3.0.tar.gz?dummy=/securemilter-lib-0.3.0.tar.gz:securemilter_lib-0.3.0-IbMuLD_GAQDiYjbvezjXRPXBsIHgc3VlerPOi6wKVW5C

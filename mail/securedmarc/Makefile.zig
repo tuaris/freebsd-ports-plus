324d45a8230a5ff1b2ea61d7d79142b6b90821e7
Daniel Morante <daniel@morante.net>
2026-07-26 03:08:31 -0400

    mail/securemilter: fix ports to use Forgejo archives + ZIG_TUPLE, add securedmarc

---
+++ mail/securedmarc/Makefile.zig
@@ -0,0 +1,1 @@
+ZIG_TUPLE=	securemilter:pacyworld.dev/securemessage/securemilter-lib/archive/v0.1.0.tar.gz?dummy=/securemilter-lib-0.1.0.tar.gz:securemilter_lib-0.1.0-IbMuLJbnAQCY0_-GTOCeYXvKIwiY6CZPRi0sJ2XsieIH
